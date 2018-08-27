1f90f2786566a7e19ba29e6b7541a903a2f9c9f0
Yiming <ymjrcc@qq.com>
2018-08-27 09:24:29 +0800

    module config

--- src/js/xq.js
+++ src/js/xq.js
@@ -1,17 +1,16 @@
 console.log('雪球关键词屏蔽器启动！');
 
+//声明各种变量
+let itemCount = 0; //前个瞬间信息块个数
+const articles = document.getElementsByClassName("timeline__item");//信息块个数
+const descrList = document.getElementsByClassName("content--description");//信息块摘要
+const detailList = document.getElementsByClassName("content--detail");//信息块全文
+const unfoldCtrList = document.getElementsByClassName("timeline__unfold__control");//信息块展开按钮
+
 //监听从 popup 发来的消息
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	// console.log('收到新的关键词列表', request.value);
-	if(request.cmd==='keywordsChange'){
-		//将 popup 传来的消息存入本地存储中
-		localStorage.setItem("xq_crx_keywords", JSON.stringify(request.value));
-		//执行一次过滤操作
-		doFilter();	
-		//告诉 popup 收到消息
-		sendResponse('keywordsChange received!');
-	}else if(request.cmd==='init'){
-		//向 popup 传入初始化 localStorage
+	if(request.cmd==='init'){//向 popup 传入初始化 localStorage
 		if(localStorage.getItem("xq_crx_keywords")){
 			chrome.runtime.sendMessage({
 				ls: localStorage.getItem("xq_crx_keywords")
@@ -21,14 +20,17 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 				}
 			});
 		}
+	}else if(request.cmd==='keywordsChange'){//将 popup 传来的关键词数组存入本地存储中
+		localStorage.setItem("xq_crx_keywords", JSON.stringify(request.value));
+		//执行一次过滤操作
+		doFilter();	
+		//告诉 popup 收到消息
+		sendResponse('keywordsChange received!');
 	}
 });
 
 
-let itemCount = 0;
-const articles = document.getElementsByClassName("timeline__item");//信息块个数
-
-const doFilter = () => {//执行过滤函数
+const doFilter = () => {//定义过滤函数
 
 	itemCount = articles.length;
 
@@ -62,11 +64,73 @@ const doFilter = () => {//执行过滤函数
 	console.log(itemCount, filter);
 }
 
+const doUnfold = () => {//定义展开函数
+	if(true){//如果设置了自动展开
+		[...detailList].forEach(i => i.style.display = 'block');
+		[...unfoldCtrList].forEach(i => {
+			i.style.display = 'block';
+			i.click();
+		});
+		[...descrList].forEach(i => i.style.display = 'none');
+	}else{//否则
+		[...descrList].forEach(i => i.style.display = 'block');
+		[...detailList].forEach(i => i.style.display = 'none');
+	}
+}
+
 setInterval(() => {
 	//如果信息块总数不变，不执行操作
 	if(articles.length===itemCount){
 		return;
 	}
-	//否则执行一次过滤
+	//否则执行一次过滤和展开判断
 	doFilter();
-}, 1000);	
+	doUnfold();
+}, 1000);
+
+
+
+//模块屏蔽
+
+//开户链接
+document.querySelector('.home__business').style.display = "none";
+
+//今日话题
+document.querySelector('.today-topic__container').style.display = "none";
+
+//雪球访谈
+document.querySelector('.talks-list__container').style.display = "none";
+
+//热股榜
+document.querySelector('.stock-hot__container').style.display = "none";
+
+//最赚钱组合
+document.querySelector('.most-profitable__container').style.display = "none";
+
+//雪球私募涨幅榜
+document.querySelector('.fund-hot-list__container').style.display = "none";
+
+//用户推荐
+document.querySelector('.recommend-user__container').style.display = "none";
+
+//悬赏提问
+document.querySelector('.home__reward__entry').style.display = "none";
+
+//其他服务
+document.querySelector('.other-service__container').style.display = "none";
+
+//举报专区
+document.querySelector('.info-report-wrap').style.display = "none";
+
+//右下角聊天
+document.querySelector('.snbim-mainview-wrap').style.display = "none";
+
+//底栏
+document.querySelector('.footer').style.display = "none";
+
+//左边栏
+document.querySelector('.user__col--lf').style.visibility = "hidden";
+
+//右边栏
+document.querySelector('.home__col--rt').style.display = "none";
+
